0ef66e43bf5b4959a74d44ee26c622a25f207d30
Johannes Hentschel <johannes.hentschel@epfl.ch>
2023-09-25 19:37:38 +0200

    Initial commit

---
+++ tests/test_skeleton.py
@@ -0,0 +1,25 @@
+import pytest
+
+from pymeasuremap.skeleton import fib, main
+
+__author__ = "Johannes Hentschel"
+__copyright__ = "Johannes Hentschel"
+__license__ = "CC0-1.0"
+
+
+def test_fib():
+    """API Tests"""
+    assert fib(1) == 1
+    assert fib(2) == 1
+    assert fib(7) == 13
+    with pytest.raises(AssertionError):
+        fib(-10)
+
+
+def test_main(capsys):
+    """CLI Tests"""
+    # capsys is a pytest fixture that allows asserts against stdout/stderr
+    # https://docs.pytest.org/en/stable/capture.html
+    main(["7"])
+    captured = capsys.readouterr()
+    assert "The 7-th Fibonacci number is 13" in captured.out
