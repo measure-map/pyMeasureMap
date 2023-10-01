612e2061701c18437b4ba0dd2f8b4538f9f2e30d
Johannes Hentschel <johannes.hentschel@epfl.ch>
2023-10-01 15:30:06 +0200

    fixes test setup

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,48 +9,38 @@
 import os.path
 from functools import cache
 from pathlib import Path
-from typing import List
+from typing import Dict
 
 import pytest
 from git import Repo
 
 from pymeasuremap.utils import collect_measure_maps
 
-REPOSITORY_PATH = "~/"
+REPOSITORY_PATH = "~/git"
 """Path where the clones of the following repositories are located:
 - https://github.com/measure-map/aligned_bach_chorales
 """
 
-
-def get_mm_paths_filepath() -> Path:
-    """An arbitrary filepath where a text files containing the paths of the individual test files is stored."""
-    return Path(__file__).parent / "all_bach_mm_paths.txt"
+# setup paths
+REPOSITORY_PATH = Path(REPOSITORY_PATH).expanduser()
+assert (
+    REPOSITORY_PATH.is_dir()
+), f"REPOSITORY_PATH is not set to an existing directory: {REPOSITORY_PATH}"
 
 
 @cache
-def mm_paths_dict() -> dict[str, Path]:
-    """Returns a dictionary mapping the name of the measure map to the path of the file containing it."""
-    mm_paths_file = get_mm_paths_filepath()
+def get_mm_path_dict() -> Dict[str, Path]:
+    """Create file with paths to all measure maps in the aligned_bach_chorales repository before setting up fixtures."""
+    aligned_bach_chorales_path = get_aligned_bach_chorales_path()
     path_dict = {}
-    with open(mm_paths_file, "r", encoding="utf-8") as f:
-        for line in f:
-            filepath = line.strip()
-            fname, _ = os.path.splitext(os.path.basename(filepath))
-            path_dict[fname] = Path(filepath)
+    for filepath in collect_measure_maps(aligned_bach_chorales_path):
+        fname, _ = os.path.splitext(os.path.basename(filepath))
+        path_dict[fname] = Path(filepath)
     return path_dict
 
 
-@pytest.fixture(scope="session")
-def repository_path() -> Path:
-    p = Path(REPOSITORY_PATH).expanduser()
-    if not p.is_dir():
-        os.makedirs(p)
-    return p
-
-
-@pytest.fixture(scope="session")
-def aligned_bach_chorales_path(repository_path) -> Path:
-    p = repository_path / "aligned_bach_chorales"
+def get_aligned_bach_chorales_path() -> Path:
+    p = REPOSITORY_PATH / "aligned_bach_chorales"
     if not p.is_dir():
         Repo.clone_from(
             "https://github.com/measure-map/aligned_bach_chorales", p, depth=1
@@ -58,19 +48,20 @@ def aligned_bach_chorales_path(repository_path) -> Path:
     return p
 
 
-@pytest.fixture(scope="session", autouse=True)
-def create_mm_paths_file(aligned_bach_chorales_path) -> List[str]:
-    """Collects the paths and writes them to a file while setting up the session so that the paths can be used to
-    parameterize tests."""
-    mm_paths = collect_measure_maps(aligned_bach_chorales_path)
-    mm_paths_file = get_mm_paths_filepath()
-    with open(mm_paths_file, "w", encoding="utf-8") as f:
-        f.write("\n".join(mm_paths))
-    yield
+def get_mm_paths_params():
+    """Used to parametrize tests."""
+    params = list(get_mm_path_dict().values())
+    print(f"len(params): {len(params)}")
+    return params
 
 
-@pytest.fixture(
-    scope="session", params=mm_paths_dict().values(), ids=mm_paths_dict().keys()
-)
+def get_mm_paths_ids():
+    """Used to parametrize tests."""
+    ids = list(get_mm_path_dict().keys())
+    print(f"len(ids): {len(ids)}")
+    return ids
+
+
+@pytest.fixture(scope="session", params=get_mm_paths_params(), ids=get_mm_paths_ids())
 def single_mm_path(request) -> Path:
     return request.param
